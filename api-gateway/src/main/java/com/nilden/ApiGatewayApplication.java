928d61384fd089943277e727c9312e196e470e39
nilden <stefan.nilden@mail.com>
2024-04-29 11:37:56 +0200

    Adding oauth project

--- api-gateway/src/main/java/com/nilden/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/nilden/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.nilden.demo;
+package com.nilden;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- api-gateway/src/main/java/com/nilden/AuthController.java
+++ api-gateway/src/main/java/com/nilden/AuthController.java
@@ -1,4 +1,4 @@
-package com.nilden.demo;
+package com.nilden;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
